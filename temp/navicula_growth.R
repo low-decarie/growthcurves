f76c95b9dfc76b2d7ef87ecfd2aac664a87f8b19
Etienne Low-Decarie <etienne.low-decarie@mail.mcgill.ca>
2014-03-21 10:05:19 -0400

    moved draft files to temp folder to avoid breaking package

---
+++ temp/navicula_growth.R
@@ -0,0 +1,14 @@
+#' Navicula growth
+#' 
+#' A dataset containing optical density through time of a culture of Navicula
+#' 
+#' \itemize{
+#'   \item Time. time since start of experiment
+#'   \item ABS optical density
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name navicula_growth
+#' @usage data(navicula_growth)
+#' @format A data frame
